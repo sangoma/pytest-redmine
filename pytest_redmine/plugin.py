985b377478c70a54ec857d833a37918e8d01d21d
Simon Gomizelj <simon@vodik.xyz>
2017-01-04 17:30:22 -0500

    Initial commit

---
+++ pytest_redmine/plugin.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+# vim:ts=4:sw=4:softtabstop=4:smarttab:expandtab
+#
+# Copyright (C) 2014  Sangoma Technologies Corp.
+# All Rights Reserved.
+# Author(s)
+# Tyler Goodlet <tgoodlet@sangoma.com>
+# Simon Gomizelj <sgomizelj@sangoma.com>
+
+import py
+import pytest
+import redmine
+from _pytest.runner import Skipped
+from ConfigParser import ConfigParser, NoOptionError, NoSectionError
+
+
+class PytestRedmineError(Exception):
+    pass
+
+
+class AssociatedIssue(Skipped):
+    def __init__(self, issue):
+        issue_title = str(issue).strip()
+        if issue_title[-1] == '.':
+           issue_title = issue_title[:-1]
+
+        msg = '{} Redmine #{}: {}'.format(issue.status.name, issue.id,
+                                          issue_title)
+
+        Skipped.__init__(self, msg)
+        self.issue = issue
+
+
+def connect_to_redmine():
+    '''Try to establish an authenticated connection to the redmine database.
+
+    Searches for login credentials in the redmine.ini configuration
+    file. First it tries using an API key and then falls back to
+    username/password
+    '''
+
+    cfg = ConfigParser()
+    cfg.read(py.path.local('redmine.ini').strpath)
+
+    try:
+        baseurl = cfg.get('redmine', 'baseurl')
+        return redmine.Redmine(baseurl, key=cfg.get('redmine', 'apikey'))
+    except NoSectionError:
+        raise PytestRedmineError("No [redmine] section found in redmine.ini")
+    except NoOptionError:
+        try:
+            return redmine.Redmine(baseurl,
+                                   username=cfg.get('redmine', 'username'),
+                                   password=cfg.get('redmine', 'password'))
+        except NoOptionError:
+            raise PytestRedmineError("Not configured with either an api key "
+                                     "or login credentials")
+
+
+def pytest_namespace():
+    return {'redmine': None}
+
+
+def pytest_configure(config):
+    # register addtional markers
+    config.addinivalue_line(
+        "markers",
+        "redmine(issueids, ...): mark to run test only if there is no pending "
+        "redmine issues for id, otherwise skip"
+    )
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_runtest_setup(item):
+    marker = item.get_marker('redmine')
+    if not marker:
+        return
+
+    if not pytest.redmine:
+        pytest.redmine = connect_to_redmine()
+
+    for issueno in marker.args:
+        issue = pytest.redmine.issue.get(issueno)
+        if issue.status.name not in ('Resolved', 'Closed'):
+            raise AssociatedIssue(issue)
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    if call.excinfo and call.excinfo.errisinstance(AssociatedIssue):
+        path, lineno, _ = item.reportinfo()
+        rep.issue = call.excinfo.value.issue
+        rep.longrepr = path, lineno, str(call.excinfo.value)
+
+
+def pytest_report_teststatus(report):
+    issue = getattr(report, 'issue', None)
+    if issue:
+        msg = '{} REDMINE'.format(issue.status.name.upper())
+        return 'skipped', '🐛', (msg, {'yellow': True})
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pytest-redmine',
+    version='1.0',
+    description='A Redmine plugin for pytest',
+    author='Simon Gomizelj',
+    author_email='sgomizelj@sangoma.com',
+    maintainer='Simon Gomizelj',
+    maintainer_email="sgomizelj@sangoma.com",
+    url='https://github.com/sangoma/pytest-redmine',
+    packages=find_packages(),
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: Implementation :: CPython',
+                 'Programming Language :: Python :: Implementation :: PyPy',
+                 'Topic :: Software Development :: Testing',
+                 'Topic :: Utilities'],
+    install_requires=[
+        'pytest>=2.6.0',
+        'python-redmine'
+    ],
+    entry_points={
+        'pytest11': ['pytest_redmine = pytest_redmine.plugin']
+    }
+)
